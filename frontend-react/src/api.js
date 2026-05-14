740f77d7f36b8c2651a5aae6d3e267eaaa44af79
spoorjith1 <spoorjith1@gmail.com>
2026-05-14 16:40:11 +0530

    backend url

--- frontend-react/src/api.js
+++ frontend-react/src/api.js
@@ -2,8 +2,10 @@ import axios from 'axios'
 import './constants'
 import { ACCESS_TOKEN } from './constants'
 
+const apiUrl = "/choreo-apis/django-react-notebook/backend-drf/v1"
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
 })
 
 api.interceptors.request.use(
